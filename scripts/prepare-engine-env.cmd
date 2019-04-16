b6222189ba82d68d74e8dfb3a4d924618dd7b30a
Rick Waldron <waldron.rick@gmail.com>
2019-04-16 11:10:23 -0400

    Add node

--- scripts/prepare-engine-env.cmd
+++ scripts/prepare-engine-env.cmd
@@ -8,7 +8,8 @@ npm run build
 npm link
 
 where engine262
-set ESHOST_ENGINE262_PATH = (where engine262) | Out-String
+set ESHOST_ENGINE262_PATH = (where engine262)
+set ESHOST_NODE_PATH = (where node)
 
 cd %ESHOST_PATH%
 
@@ -20,12 +21,14 @@ jsvu --os=win64 --engines="chakra,spidermonkey,v8,xs"
 set PATH="%PATH%;%USERPROFILE%\.jsvu\"
 set ESHOST_PATH_CHAKRA="%USERPROFILE%\.jsvu\chakra.cmd"
 set ESHOST_PATH_ENGINE262="%ESHOST_ENGINE262_PATH%"
+set ESHOST_PATH_NODE="%ESHOST_NODE_PATH%"
 set ESHOST_PATH_SPIDERMONKEY="%USERPROFILE%\.jsvu\spidermonkey.cmd"
 set ESHOST_PATH_V8="%USERPROFILE%\.jsvu\v8.cmd"
 set ESHOST_PATH_XS="%USERPROFILE%\.jsvu\xs.cmd"
 
 eshost --add "chakra" ch %ESHOST_PATH_CHAKRA%
 eshost --add "engine262" engine262 %ESHOST_PATH_ENGINE262%
+eshost --add "node" node %ESHOST_PATH_NODE%
 eshost --add "spidermonkey" jsshell %ESHOST_PATH_SPIDERMONKEY%
 eshost --add "v8" d8 %ESHOST_PATH_V8%
 eshost --add "xs" xs %ESHOST_PATH_XS%
--- scripts/prepare-engine-env.ps1
+++ scripts/prepare-engine-env.ps1
@@ -12,6 +12,7 @@ npm link
 where engine262
 
 $Env:ESHOST_ENGINE262_PATH = (where engine262) | Out-String
+$Env:ESHOST_NODE_PATH = (where node) | Out-String
 
 cd $Env:ESHOST_PATH
 # npm install --ignore-scripts
@@ -26,13 +27,15 @@ dir $Env:USERPROFILE\.jsvu\
 
 $Env:PATH="$Env:PATH;$Env:USERPROFILE\.jsvu\"
 $Env:ESHOST_PATH_CHAKRA="$Env:USERPROFILE\.jsvu\chakra.cmd"
-$Env:ESHOST_PATH_ENGINE262="C:\Users\appveyor\AppData\Roaming\npm\engine262.cmd"
+$Env:ESHOST_PATH_ENGINE262="$Env:ESHOST_ENGINE262_PATH"
+$Env:ESHOST_PATH_NODE="$Env:ESHOST_NODE_PATH"
 $Env:ESHOST_PATH_SPIDERMONKEY="$Env:USERPROFILE\.jsvu\spidermonkey.cmd"
 $Env:ESHOST_PATH_V8="$Env:USERPROFILE\.jsvu\v8.cmd"
 $Env:ESHOST_PATH_XS="$Env:USERPROFILE\.jsvu\xs.cmd"
 
 eshost --add "chakra" ch $Env:ESHOST_PATH_CHAKRA
 eshost --add "engine262" engine262 $Env:ESHOST_PATH_ENGINE262
+eshost --add "node" node $Env:ESHOST_NODE_PATH
 eshost --add "spidermonkey" jsshell $Env:ESHOST_PATH_SPIDERMONKEY
 eshost --add "v8" d8 $Env:ESHOST_PATH_V8
 eshost --add "xs" xs $Env:ESHOST_PATH_XS
