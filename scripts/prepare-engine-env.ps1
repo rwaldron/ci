6c64d2325584b1f553dbcccba22dc8996bc3d7c1
Rick Waldron <waldron.rick@gmail.com>
2019-04-16 10:33:50 -0400

    Remove dependencies from package.json; install global deps directly from scripts

--- scripts/prepare-engine-env.ps1
+++ scripts/prepare-engine-env.ps1
@@ -11,9 +11,12 @@ $Env:ESHOST_ENGINE262_PATH="C:\Users\appveyor\AppData\Roaming\npm\"
 $Env:PATH="$Env:PATH;C:\Users\appveyor\AppData\Roaming\npm\"
 where engine262
 cd $Env:ESHOST_PATH
-npm install --ignore-scripts
+# npm install --ignore-scripts
 # - npm link
-.\node_modules\.bin\jsvu --os=win64 --engines="chakra,spidermonkey,v8,xs"
+
+npm install -g jsvu eshost
+
+jsvu --os=win64 --engines="chakra,spidermonkey,v8,xs"
 
 dir C:\Users\appveyor\AppData\Roaming\npm\
 dir $Env:USERPROFILE\.jsvu\
@@ -25,8 +28,8 @@ $Env:ESHOST_PATH_SPIDERMONKEY="$Env:USERPROFILE\.jsvu\spidermonkey.cmd"
 $Env:ESHOST_PATH_V8="$Env:USERPROFILE\.jsvu\v8.cmd"
 $Env:ESHOST_PATH_XS="$Env:USERPROFILE\.jsvu\xs.cmd"
 
-.\node_modules\.bin\eshost --add "chakra" ch $Env:ESHOST_PATH_CHAKRA
-.\node_modules\.bin\eshost --add "engine262" engine262 $Env:ESHOST_PATH_ENGINE262
-.\node_modules\.bin\eshost --add "spidermonkey" jsshell $Env:ESHOST_PATH_SPIDERMONKEY
-.\node_modules\.bin\eshost --add "v8" d8 $Env:ESHOST_PATH_V8
-.\node_modules\.bin\eshost --add "xs" xs $Env:ESHOST_PATH_XS
+eshost --add "chakra" ch $Env:ESHOST_PATH_CHAKRA
+eshost --add "engine262" engine262 $Env:ESHOST_PATH_ENGINE262
+eshost --add "spidermonkey" jsshell $Env:ESHOST_PATH_SPIDERMONKEY
+eshost --add "v8" d8 $Env:ESHOST_PATH_V8
+eshost --add "xs" xs $Env:ESHOST_PATH_XS
